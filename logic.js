38451fca54735ca6720a9e59cf4d5f1d987ab17d
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-01 20:53:18 -0400

    Added moment function and got the current day and time to print to the page

--- logic.js
+++ logic.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
+var now = moment().format("dddd");
+console.log(now);
+$(".lead").text("Today is: " + now);
+$("#currentDay").text("The local time is: " + moment().format("h:mm a"));
 
 })
