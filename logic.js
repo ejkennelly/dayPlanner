8fef93a858fcc163283873aaf5b554b9c7c471ba
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-02 00:47:17 -0400

    Added for loop to change the color of the rows depending on the time

--- logic.js
+++ logic.js
@@ -1,7 +1,53 @@
-$(document).ready(function(){
-var now = moment().format("dddd");
-console.log(now);
-$(".lead").text("Today is: " + now);
-$("#currentDay").text("The local time is: " + moment().format("h:mm a"));
+var currentDate = moment().format("ddd, MMMM Do");
+var localTime = moment().format("h:mm a");
+var saveBtn = $(".saveBtn");
+var currentTime = moment().format("HH");
+var currentTimeInt = parseInt(currentTime);
+
+$("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#10Row").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
+$("#11Row").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
+$("#12Row").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
+$("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+
+$(document).ready(function () {
+
+    renderPlans();
+    //Placing current date and time at top of page
+    $(".lead").text("Today is: " + currentDate);
+    $("#currentDay").text("The local time is: " + localTime);
+
+    //for loop to update the time and change row colors
+    for (var i = 0; i <= 12; i++) {
+        var inputTime = $("#" + i + "Row").attr("data-time");
+        var inputTimeInt = parseInt(inputTime);
+        console.log(inputTimeInt);
+
+        if (currentTimeInt === inputTimeInt) {
+            $("#" + i + "Row").addClass("present");
+        }
+        if (currentTimeInt < inputTimeInt) {
+            $("#" + i + "Row").addClass("future");
+        }
+        if (currentTimeInt > inputTimeInt) {
+            $("#" + i + "Row").addClass("past")
+        }
+    }
+
+    // button hover
+    saveBtn.on("mouseenter", function() {
+        $(this).addClass("hover");
+    });
+
+    saveBtn.on("mouseleave", function() {
+        $(this).removeClass("hover");
+    });
+
+    // button click
 
 })
